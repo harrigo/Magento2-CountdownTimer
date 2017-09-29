d031540a5ab14bf04d4cfeb01ac07ebfaaabeae5
Harri <Harri.g.owen@gmail.com>
2017-09-29 16:14:26 +0100

    Added configuration values to block

--- Block/DeliveryCountdown.php
+++ Block/DeliveryCountdown.php
@@ -3,9 +3,22 @@ namespace Harrigo\DeliveryCountdown\Block;
 
 class DeliveryCountdown extends \Magento\Framework\View\Element\Template 
 {
+	protected $scopeConfig;
+	
+	public function __construct(
+		\Magento\Framework\View\Element\Template\Context $context,
+		\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    ) {
+		$this->scopeConfig = $scopeConfig;
+		parent::__construct(
+            $context
+        );
+    }
+	
 	public function getDeliveryDate() {
+		$deliverydaysadmin = $this->scopeConfig->getValue('deliverycountdown/delivery/deliverytime', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
 		$currenttime = $this->getCurrentTime();
-		$deliverydate = date('d/m/Y', strtotime(date('d-m-Y') . ' + 2 weekdays'));
+		$deliverydate = date('d/m/Y', strtotime(date('d-m-Y') . ' + ' . $deliverydaysadmin . ' weekdays'));
 		//if ($currenttime >= $this->getCufOffTime()) {
 			//$deliverydate+= 86400;
 		//}
@@ -13,8 +26,9 @@ class DeliveryCountdown extends \Magento\Framework\View\Element\Template
 	}
 	
 	public function getCutOffTime() {
+		$cuttoffadmin = $this->scopeConfig->getValue('deliverycountdown/delivery/cutofftime', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
 		$currenttime = $this->getCurrentTime();
-		$cutofftime = strtotime(date('h:i:s A', strtotime(date('d-m-Y') . ' + 16 hours')));
+		$cutofftime = strtotime(date('h:i:s A', strtotime(date('d-m-Y') . ' + ' . $cuttoffadmin . ' hours')));
 		//if ($currenttime>$cutofftime) {
 			//$cutofftime+= 86400;
 		//}
